f016d9ad804e1b69bbd3c7c76685dccec0a2ce6d
Finn Behrens <kloenk@gmx.de>
2017-06-16 11:54:40 +0200

    added a basis view and connect text input fields

--- DieMachenNurDieBeine/ViewController.swift
+++ DieMachenNurDieBeine/ViewController.swift
@@ -2,23 +2,27 @@
 //  ViewController.swift
 //  DieMachenNurDieBeine
 //
-//  Created by admin on 16.06.17.
+//  Created by Finn Behrens on 16.06.17.
 //  Copyright © 2017 CrashCurs. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Outlet
+    @IBOutlet weak var RechnungsTextFeld: UITextField!
+    @IBOutlet weak var TrinkGeldTextFeld: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }*/
 
 
 }
